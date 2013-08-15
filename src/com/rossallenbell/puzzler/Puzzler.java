2c76769c96ea04076f179c9dd1ce5282f6515eea
Ross Bell <rossallenbell@gmail.com>
2013-08-14 21:30:27 -0500

    Initial commit

---
+++ src/com/rossallenbell/puzzler/Puzzler.java
@@ -0,0 +1,11 @@
+package com.rossallenbell.puzzler;
+
+import java.awt.image.BufferedImage;
+
+public class Puzzler {
+    
+    public Puzzle puzzlefy(BufferedImage image) {
+        return new Puzzle();
+    }
+    
+}
