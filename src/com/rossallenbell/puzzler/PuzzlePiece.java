98a6ac246fb3fdeeb943111c3b0b9fe75ab82ef6
Ross Bell <rossallenbell@gmail.com>
2013-09-11 19:41:29 -0500

    Finally figured out the inverse edge cutting logic

--- src/com/rossallenbell/puzzler/PuzzlePiece.java
+++ src/com/rossallenbell/puzzler/PuzzlePiece.java
@@ -4,6 +4,7 @@ import java.awt.AlphaComposite;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.util.HashMap;
 import java.util.Map;
@@ -82,33 +83,51 @@ public class PuzzlePiece {
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         
         for (DIR dir : DIR.values()) {
-            BufferedImage featureImage = PuzzlePieceEdge.FEATURE_IMAGES.get(puzzlePiece.getEdge(dir).getFeature());
-            BufferedImage scaledFeatureImage = new BufferedImage(pieceBoundsImage.getWidth(), pieceBoundsImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
-            Graphics2D gF = scaledFeatureImage.createGraphics();
-            AffineTransform at = new AffineTransform();
+            BufferedImage scaledFeatureImage = scaleImage(PuzzlePieceEdge.FEATURE_IMAGES.get(puzzlePiece.getEdge(dir).getFeature()), pieceBoundsImage.getWidth(), pieceBoundsImage.getHeight());
+            AffineTransform rotateToDir = new AffineTransform();
+            double rotation = dir.getRotation();
+            AlphaComposite includeOrExcludeFeatureShape = AlphaComposite.getInstance(AlphaComposite.DST_IN);
             
-            at.translate(scaledFeatureImage.getWidth() / 2, scaledFeatureImage.getHeight() / 2);
-            at.rotate(dir.getRotation());
-            at.translate(-scaledFeatureImage.getWidth() / 2, -scaledFeatureImage.getHeight() / 2);
-            at.scale(scaledFeatureImage.getWidth() / (double) featureImage.getWidth(), scaledFeatureImage.getHeight() / (double) featureImage.getHeight());
             
-            AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.DST_IN);
             if (puzzlePiece.getEdge(dir).isInverted()) {
-                at.scale(1, -1);
-                at.translate(0, edgeThickness * -8); //can't figure out why the mulitplier is 8 and not 2
+                AffineTransform dropDown = new AffineTransform();
+                dropDown.translate(0, pieceBoundsImage.getWidth() - (edgeThickness * 2));
+                BufferedImage tempBI = new BufferedImage(scaledFeatureImage.getWidth(), scaledFeatureImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
+                Graphics2D tempG = tempBI.createGraphics();
+                tempG.drawImage(scaledFeatureImage, dropDown, null);
+                tempG.dispose();
+                scaledFeatureImage = tempBI;
                 
-                ac = AlphaComposite.getInstance(AlphaComposite.DST_OUT);
+                rotation += Math.PI;
+                rotation = rotation % (2 * Math.PI);
+                includeOrExcludeFeatureShape = AlphaComposite.getInstance(AlphaComposite.DST_OUT);
             }
             
-            gF.drawImage(featureImage, at, null);
+            rotateToDir.translate(pieceBoundsImage.getWidth() / 2, pieceBoundsImage.getHeight() / 2);
+            rotateToDir.rotate(rotation);
+            rotateToDir.translate(-pieceBoundsImage.getWidth() / 2, -pieceBoundsImage.getHeight() / 2);
+            
+            
+            BufferedImage rotatedFeatureImage = new BufferedImage(pieceBoundsImage.getWidth(), pieceBoundsImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
+            Graphics2D gF = rotatedFeatureImage.createGraphics();
+            gF.drawImage(scaledFeatureImage, rotateToDir, null);
             gF.dispose();
-            g.setComposite(ac);
-            g.drawImage(scaledFeatureImage, 0, 0, scaledFeatureImage.getWidth(), scaledFeatureImage.getHeight(), null);
+            g.setComposite(includeOrExcludeFeatureShape);
+            g.drawImage(rotatedFeatureImage, 0, 0, rotatedFeatureImage.getWidth(), rotatedFeatureImage.getHeight(), null);
         }
         
         g.dispose();
         
         return pieceBoundsImage;
+    }
+    
+    private static BufferedImage scaleImage(BufferedImage image, int width, int height) {
+        int originalWidth = image.getWidth();
+        int originalHeight = image.getHeight();
+        AffineTransform at = new AffineTransform();
+        at.scale(width / (double) originalWidth, height / (double) originalHeight);
+        AffineTransformOp scaleOp = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
+        return scaleOp.filter(image, null);
     }
     
     public BufferedImage getImage() {
